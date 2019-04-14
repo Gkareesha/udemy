a6ecdc2eb9a14295e4ab47b88584a8950e28f7d7
Carina <truefaya@gmail.com>
2019-04-14 09:28:07 +0300

    My first try

---
+++ index.js
@@ -0,0 +1,15 @@
+'use strict';
+
+let persone = {
+    name: "Frank",
+    age: 27,
+    isMarried: true
+};
+
+console.log(persone['isMarried']);
+
+
+// alert("Hello World!");
+let answer = prompt("Есть ли Вам 18?", "Да");
+
+console.log(answer);
