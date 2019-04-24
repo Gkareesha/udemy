e6a08f414924019d2b26b38e3c58623dc9b9103e
Carina <truefaya@gmail.com>
2019-04-24 18:01:14 +0300

    lesson 3

--- tempCodeRunnerFile.js
+++ tempCodeRunnerFile.js
@@ -1,7 +1,63 @@
-let persone = {
-    name: "Frank",
-    age: 27,
-    isMarried: true
+let money, time;
+
+function start() {
+    money = +prompt("Ваш бюджет на месяц?", "");
+    time = prompt("Введите дату в формате YYYY-MM-DD", "");
+
+    while (isNaN(money) || money == '' || money == null) {
+        money = +prompt("Ваш бюджет на месяц?", "");
+    }
+}
+
+start();
+
+let appData = {
+    budget: money,
+    timeData: time,
+    expenses: {},
+    optionalExpenses: {},
+    income: [],
+    savings: true
 };
 
-console.log(persone['name']);
+
+function chooseExpences() {
+    for (let i = 0; i < 2; i++) {
+        let a = prompt("Введите обязательную статью расходов в этом месяце", ""),
+            b = prompt("Во сколько обойдется?", "");
+
+        if (typeof(a) === 'string' && typeof(a) != null && typeof(b) != null && a != "" && b != "" && a.length < 50) {
+
+            console.log("done");
+
+            appData.expenses[a] = b;
+        } else {
+            console.log("bad result");
+            i--;
+        }
+
+    };
+}
+
+chooseExpences();
+
+
+function checkSavings() {
+    if (appData.savings == true) {
+        let save = +prompt("Сумма депозита?"),
+            percent = +prompt("Под какой процент?");
+
+        appData.monthIncome = save / 100 / 12 * percent;
+        alert("Доход с депозита: " + appData.monthIncome);
+    }
+}
+
+checkSavings();
+
+function detectDayBudget() {
+    appData.moneyPerDay = (appData.budget / 30).toFixed();
+    //alert("Бюджет на 1 день составляет " + appData.moneyPerDay + "руб.");
+    console.log("Бюджет на 1 день составляет " + appData.moneyPerDay + "руб.");
+}
+
+detectDayBudget();
